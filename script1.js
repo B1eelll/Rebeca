99a5aeafca7dd86f37252b9b2e7a91ba360eb073
Gabriel Alcântara <gabrielalcantaraproj@gmail.com>
2025-01-14 18:03:25 -0300

    Commit inicial de "amoreco"

---
+++ script1.js
@@ -0,0 +1,50 @@
+let casalBebendo = document.getElementById('casalbebendo')
+let casalAbraco = document.getElementById('casalabraco')
+let rebeijinho = document.getElementById('rebeijinho')
+let proxp = document.getElementById('proxp')
+let comida = document.getElementById('comidapreferida')
+let btnsenha = document.getElementById('btnsenha')
+
+//EVENTOS DE IMAGENS:
+    casalBebendo.addEventListener('click',function(){            
+        window.alert("Você nem se lembrava dessa foto, né?")})     
+    
+    casalAbraco.addEventListener('click',function(){
+        window.alert("Abraço gostooooooooooooooso!")})
+    
+    rebeijinho.addEventListener('click',function(){
+        window.alert("😘😘😘😘😘😘😘😘😘😘")})
+
+    comida.addEventListener('click', function(){
+        window.alert("Achou mesmo que eu iria esquecer de: Batatinha, Feijão, Açaí, Pipoca, Limonada e Peixe frito?")
+    })
+    //
+
+        
+//EVENTOS DE BOTÕES
+    proxp.addEventListener('mouseenter' ,function(){
+        proxp.style.backgroundColor ='pink'})
+
+    proxp.addEventListener('mouseleave',function(){
+        proxp.style.backgroundColor = 'white'})
+        
+    proxp.addEventListener('click',function(){
+        window.location.href = 'index2.html';
+    });
+    btnsenha.addEventListener('mouseenter', function(){
+        btnsenha.style.backgroundColor = 'gray'});
+    btnsenha.addEventListener('mouseleave' , function(){
+        btnsenha.style.backgroundColor = 'white'});
+// 
+
+    btnsenha.addEventListener('click', function senha(){
+        let senha = window.prompt('Digite a senha super secreta:')
+        if (senha != 220479){
+            window.alert("Senha incorreta, Presta atenção Cabeçuda.")
+        } else {
+            window.confirm('Você tem um Desejo! Pode me pedir oque quiser. Não precisa se acanhar por nada!')
+        }
+    })
+
+window.alert('Fiz esse site Horroroso com todo o carinho pra você, espero que goste! ❤️')
+
---
+++ script2.js
@@ -0,0 +1,5 @@
+let btnvoltar = document.getElementById('btnvoltar')
+
+btnvoltar.addEventListener('click', function(){
+    window.location.href = 'index1.html';
+})

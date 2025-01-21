7f4da88f665028efa1e6058186736bf7a3d2c11d
Gabriel Alcântara <gabrielalcantaraproj@gmail.com>
2025-01-20 22:22:00 -0300

    Conserto temporário segunda página.

--- script2.js
+++ script2.js
@@ -3,3 +3,7 @@ let btnvoltar = document.getElementById('btnvoltar')
 btnvoltar.addEventListener('click', function(){
     window.location.href = 'index.html';
 })
+
+
+
+window.alert('Desculpa meu amor, vem ficar comigo no seu aniversário... ')
